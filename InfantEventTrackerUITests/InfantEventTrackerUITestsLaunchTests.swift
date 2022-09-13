7ccd5c924d7c57785c84c60afdf38fda89d513c7
Andy <andrewschools@me.com>
2022-09-13 13:03:17 -0400

    Adding Apple Watch preview.

---
+++ InfantEventTrackerUITests/InfantEventTrackerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  InfantEventTrackerUITestsLaunchTests.swift
+//  InfantEventTrackerUITests
+//
+//  Created by Andrew Schools on 9/13/22.
+//  Copyright © 2022 Andrew Schools. All rights reserved.
+//
+
+import XCTest
+
+class InfantEventTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
